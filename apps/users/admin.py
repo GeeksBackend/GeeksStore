fae870d6bed2562297053dff28158d6d54d8dadf
Toktorov <ktoktorov2005@gmail.com>
2024-04-20 19:11:46 +0600

    add user register

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from apps.users.models import User
+
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'last_name', 'first_name', 'date_joined')
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+
+from apps.users.models import User
+from apps.users.serializers import UserSerializer, UserRegisterSerializer
 
 # Create your views here.
+class UserAPI(ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_serializer_class(self):
+        print(self.request.method == "POST")
+        if self.request.method == "POST":
+            return UserRegisterSerializer
+        return UserSerializer
