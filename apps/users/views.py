f4091dcde9c6fe7383c3efcc403449468fa6531a
Toktorov <ktoktorov2005@gmail.com>
2024-04-27 20:55:01 +0600

    add permissions users

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,8 +1,10 @@
 from rest_framework.viewsets import GenericViewSet
 from rest_framework import mixins
+from rest_framework.permissions import AllowAny
 
 from apps.users.models import User
 from apps.users.serializers import UserSerializer, UserRegisterSerializer
+from apps.users.permissions import UserPermission
 
 # Create your views here.
 class UserAPI(GenericViewSet,
@@ -19,3 +21,8 @@ class UserAPI(GenericViewSet,
         if self.request.method == "POST":
             return UserRegisterSerializer
         return UserSerializer
+    
+    def get_permissions(self):
+        if self.request.method in ('PUT', 'PATCH', 'DELETE'):
+            return (UserPermission(), )
+        return (AllowAny(), )
